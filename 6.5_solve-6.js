a540d0f7c624336be9c2f143ebc70e0a9325cf5c
abusalehjahid007 <abusalehjahid007@gmail.com>
2025-04-26 17:33:21 +0600

    solve 6

---
+++ 6.5_solve-6.js
@@ -0,0 +1,16 @@
+const input = require('prompt-sync')();
+
+// question 5 solve :
+
+const large_name = (n) => {
+    let large  = n[0];
+    for(let i of n){
+        if(i.length >= large.length){
+            large = i;
+        }
+    }
+    return large;
+}
+
+let friends = ["rahim", "karim", "abdul", "sadsd", "heroAlom"];
+console.log(large_name(friends));
